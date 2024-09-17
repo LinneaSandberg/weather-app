1fd8419028bd0a2a155b238bd1fe5a56ea2ee153
Linnea Sandberg <llinnea.sandberg@gmail.com>
2024-09-17 12:30:43 +0200

    Made a geogoding fetch to get the longitud and latitude from user searchinput

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 // here be imports of components
+import OptionsBar from './components/OptionsBar.vue'
 
 </script>
 
 <template>
   <header>
-
+    <h1>My App</h1>
+    <OptionsBar />
   </header>
 
   <main>
