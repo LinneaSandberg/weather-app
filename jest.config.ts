dae23fc7ca7507939d89ce3df7ecaa8fed9673cc
Linnea Sandberg <llinnea.sandberg@gmail.com>
2024-09-18 13:13:51 +0200

    setup for jest testing, and simple test to see so it works.

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from "jest";
+
+const config: Config = {
+	preset: "ts-jest",
+	testEnvironment: "jest-environment-jsdom",
+	transform: {
+		"^.+\\.jsx?$": "babel-jest",
+		"^.+\\.vue$": "@vue/vue3-jest",
+	},
+	testEnvironmentOptions: {
+		customExportConditions: ["node", "node-addons"],
+	},
+};
+
+export default config;
--- src/components/OptionsBar.vue
+++ src/components/OptionsBar.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import { ref, watch, onMounted } from "vue";
 import WeatherDisplay from "./WeatherDisplay.vue";
 import axios from "axios";
@@ -9,7 +9,7 @@ const latitude = ref(null);
 const longitude = ref(null);
 
 const fetchLocationData = async (newCity) => {
-	console.log(`New city: ${newCity}`);
+	// console.log(`New city: ${newCity}`);
 
 	if (!newCity) {
 		return;
@@ -21,11 +21,11 @@ const fetchLocationData = async (newCity) => {
 			`https://geocoding-api.open-meteo.com/v1/search?name=${newCity}&count=1&language=en&format=json`
 		);
 		// const responseData = await res.json();
-		console.log("res: ", res.data);
+		// console.log("res: ", res.data);
 
 		if (res.data.results && res.data.results.length > 0) {
-			console.log("latitude:", res.data.results[0].latitude);
-			console.log("longitude:", res.data.results[0].longitude);
+			// console.log("latitude:", res.data.results[0].latitude);
+			// console.log("longitude:", res.data.results[0].longitude);
 
 			latitude.value = res.data.results[0].latitude;
 			longitude.value = res.data.results[0].longitude;
@@ -36,13 +36,13 @@ const fetchLocationData = async (newCity) => {
 };
 
 watch(city, (newCity) => {
-	console.log(`New city: ${newCity}`);
+	// console.log(`New city: ${newCity}`);
 	fetchLocationData(newCity);
 });
 
 onMounted(() => {
 	const defaultCity = "Berlin";
-	console.log("Component mounted");
+	// console.log("Component mounted");
 	city.value = defaultCity;
 	fetchLocationData(defaultCity);
 });
