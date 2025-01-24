90ebcfefe6958087a0515f485937a39d9f5db643
Linnea Sandberg <llinnea.sandberg@gmail.com>
2025-01-24 13:30:37 +0100

    Moved build .js files to the .dist directory.

--- src/main.js
+++
@@ -1,4 +0,0 @@
-import "./scss/main.scss";
-import { createApp } from "vue";
-import App from "./App.vue";
-createApp(App).mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,9 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue(), vueJsx()],
-	base: "/weather-app/",
+	base: "/",
 	resolve: {
 		alias: {
 			"@": fileURLToPath(new URL("./src", import.meta.url)),
