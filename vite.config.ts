fd27d022c4e4985aae4dbb0f40557cacfcb81b8e
Linnea Sandberg <llinnea.sandberg@gmail.com>
2025-03-10 03:18:39 +0100

    Changed configuration to point to dist.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
-import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
 
 export default defineConfig({
-	plugins: [vue(), vueJsx()],
-	base: "/",
-	resolve: {
-		alias: {
-			"@": fileURLToPath(new URL("./src", import.meta.url)),
-		},
+	plugins: [vue()],
+	build: {
+		outDir: "./dist",
 	},
 });
